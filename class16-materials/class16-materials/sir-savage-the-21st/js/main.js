b3fa79b6d832e7773ec134a48296e0072b11b996
Karen Du <karenkaydu@gmail.com>
2023-08-04 16:53:57 -0700

    Complete sir-savage exercise

--- class16-materials/class16-materials/sir-savage-the-21st/js/main.js
+++ class16-materials/class16-materials/sir-savage-the-21st/js/main.js
@@ -1,10 +1,14 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
 
-function twentyOneTimes() {
-    for (let i = 1; i <= 21; i++){
-        document.querySelector('#savageSays').innerText += ' 21'
+
+
+function print21(i) {
+    for (let i = 1; i < 22; i++) {
+        // console.log(' 21')
+        document.querySelector('#savageSays').innerText += ' 21 '
     }
 }
 
-twentyOneTimes()
+print21(21)
+
