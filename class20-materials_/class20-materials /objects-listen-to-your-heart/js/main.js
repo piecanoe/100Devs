975ca2f3946b4c45ed11bc7afd3ec881d2d504dd
Karen Du <karenkaydu@gmail.com>
2023-08-07 12:26:13 -0700

    Complete 'listen-to-your-heart' and 'winter-games'

--- class20-materials_/class20-materials /objects-listen-to-your-heart/js/main.js
+++ class20-materials_/class20-materials /objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,20 @@
 //Create a stopwatch object that has four properties and three methods
+
+let skateboard = {}
+
+skateboard.size = '8.0'
+skateboard.color = 'green stripe'
+skateboard.style = 'double nose'
+skateboard.material = 'wood'
+
+skateboard.pop = function(){
+    console.log('You popped the board super high!')
+}
+
+skateboard.measure = function(size){
+    console.log(`My board is a ${size}`)
+}
+
+skateboard.slip = function(){
+    console.log('it\'s getting away!!!')
+}
--- class20-materials_/class20-materials /review-winter-games/js/main.js
+++ class20-materials_/class20-materials /review-winter-games/js/main.js
@@ -1,21 +1,21 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array 
 
-// function onlyEvensFinder(arr){
-//     let onlyEvens = []
-//     arr.forEach (num => {
-//         if (num % 2 === 0){
-//             onlyEvens.push(num)
-//          }
-//     })
-//     return onlyEvens
-// }
 
-// console.log(onlyEvensFinder([1, 2, 33, 44, 52, 4])) 
+function can(arr){
+    let onlyEvens = []
+    arr.forEach (num => {
+        if (num % 2 === 0){
+            onlyEvens.push(num)
+        }
+    })
+    return onlyEvens
+}
+
+
+console.log(can([3, 5, 16, 66, 36]))
+
+
+
 
 // Bonus: use map or filter)
 
-function onlyEvensFinder(arr){
-    return arr.filter( num => num % 2 === 0)
-}
-
-console.log(onlyEvensFinder([1, 2, 33, 44, 52, 4])) 
