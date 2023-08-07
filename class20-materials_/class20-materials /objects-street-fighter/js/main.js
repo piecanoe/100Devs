7528c9e845927e87271290f8a0784e67c6996d00
Karen Du <karenkaydu@gmail.com>
2023-08-07 12:49:27 -0700

    Complete objects-street-fighter practice

--- class20-materials_/class20-materials /objects-street-fighter/js/main.js
+++ class20-materials_/class20-materials /objects-street-fighter/js/main.js
@@ -1,1 +1,23 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+
+function StreetFighter (name, gender, type, nationality){
+    this.name = name
+    this.gender = gender
+    this.type = type
+    this.nationality = nationality
+
+    this.intro = function(name){
+        console.log(`Hi! Call me ${this.name}`)
+    }
+
+    this.special = function(){
+        console.log(`${this.name} just did their special move!`)
+    }
+
+    this.die = function(){
+        console.log('You died.')
+    }
+}
+
+let guile = new StreetFighter ('Guile', "M", "Fighting", "USA")
